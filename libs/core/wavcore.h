a3c8443b4479f14bde7de622d53be1c46a1960c3
root <root@idev.(none)>
2014-01-13 10:53:09 +0800

    added wavcore.*

---
+++ libs/core/wavcore.h
@@ -0,0 +1,10 @@
+#ifndef __WAVCORE_H__
+#define __WAVCORE_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+int simwav(char *needleFile, char *haystackFile, int64_t *records)
+#ifdef __cplusplus
+};
+#endif
+#endif
